359422d2f871a1e0f9fa6fd231287dc100e2c535
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-30 15:13:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ end
 
 def counts_number_of_bears_with_goofy_temperaments
   "Write your SQL query here"
-  "SELECT COUNT(bears.temperament)"
+  "SELECT COUNT(bears.temperament) FROM bears WHERE bears.temperament= 'goofy' "
 end
 
 def selects_bear_that_killed_Tim
