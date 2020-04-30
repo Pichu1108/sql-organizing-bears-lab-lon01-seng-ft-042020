61e0c8389c5f97f1af6b7fa03c6f5e8295de0f6f
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-30 15:12:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,10 +25,12 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
+
 end
 
 def counts_number_of_bears_with_goofy_temperaments
   "Write your SQL query here"
+  "SELECT  COUNT(bears.temperament)"
 end
 
 def selects_bear_that_killed_Tim
